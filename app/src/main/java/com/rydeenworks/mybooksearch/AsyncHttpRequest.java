0c4f925eb060d071453f11746896179ff3006e5b
rydeenworks <rydeenworks@gmail.com>
2019-07-25 06:12:18 +0900

    implement calilSearch

---
+++ app/src/main/java/com/rydeenworks/mybooksearch/AsyncHttpRequest.java
@@ -0,0 +1,46 @@
+package com.rydeenworks.mybooksearch;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AsyncHttpRequest extends AsyncTask<String, Void, String> {
+    private MainActivity mActivity;
+
+    public AsyncHttpRequest(MainActivity activity) {
+        mActivity = activity;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        HttpURLConnection connection = null;
+        StringBuilder src = new StringBuilder();
+        try {
+            URL url = new URL(params[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            InputStream is = connection.getInputStream();
+
+            while (true) {
+                byte[] line = new byte[1024];
+                int size = is.read(line);
+                if (size <= 0)
+                    break;
+                String str = new String(line, "utf-8");
+                src.append(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            connection.disconnect();
+        }
+        return new String(src);
+    }
+
+    public void onPostExecute(String string) {
+        mActivity.OnLoadBookHttp(string);
+    }
+}
