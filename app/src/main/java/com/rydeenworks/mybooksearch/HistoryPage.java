368ce1de15641daa7bd6171afa05fc04d4e7a9d7
rydeenworks <rydeenworks@gmail.com>
2019-12-31 15:22:22 +0900

    implement book image page. #22

--- app/src/main/java/com/rydeenworks/mybooksearch/HistoryPage.java
+++ app/src/main/java/com/rydeenworks/mybooksearch/HistoryPage.java
@@ -170,4 +170,58 @@ public class HistoryPage {
 
         return htmlBuilder.toString();
     }
+
+    public String GetImagePage (Context context, int view_width) {
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getString(R.string.preference_history_file_key), Context.MODE_PRIVATE);
+
+        ArrayList<JSONArray> historyList = getHistoryList(context, sharedPref);
+        return createImageHtml(context, historyList, view_width);
+    }
+
+
+    private String createImageHtml(Context context, ArrayList<JSONArray> historyList, int viewWidth) {
+        StringBuilder htmlBuilder = new StringBuilder();
+
+        htmlBuilder.append("<html>");
+        htmlBuilder.append("<head>");
+        htmlBuilder.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
+        htmlBuilder.append("<style type=\"text/css\">");
+        htmlBuilder.append(".header {");
+        htmlBuilder.append("background-color: #bdf7f1;");
+        htmlBuilder.append("}");
+        htmlBuilder.append("</style>");
+        htmlBuilder.append("<style>img{display: inline;height: auto;max-width: 18%;}</style>");
+        htmlBuilder.append("</head>");
+
+        htmlBuilder.append("<body>");
+        try {
+            int count=0;
+            final int BOOK_NUM = 5;
+            for (JSONArray jsonArray : historyList) {
+                if(count==0) {
+
+                    htmlBuilder.append("<div style=\"text-align:center;\">");
+                }
+                htmlBuilder.append("<img src=\"");
+                String url = "https://cover.openbd.jp/" + jsonArray.get(1) + ".jpg\" ";
+                htmlBuilder.append(url);
+                htmlBuilder.append("alt=\"" + jsonArray.get(0) + "\" style=\"border:solid 1px #000000\"");
+                htmlBuilder.append(">");
+                count++;
+                if(count == BOOK_NUM){
+                    htmlBuilder.append("</div>");
+                    count = 0;
+                }
+            }
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            return "";
+        }
+
+        htmlBuilder.append("</body>");
+        htmlBuilder.append("</html>");
+
+        return htmlBuilder.toString();
+    }
 }
