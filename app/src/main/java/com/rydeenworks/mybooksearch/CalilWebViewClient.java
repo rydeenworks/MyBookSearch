fe2b35105c5879e65b90b644eba5260c067859fb
RydeenWorks <rydeenworks@gmail.com>
2023-01-01 11:25:16 +0900

    deprecatedなAPIの警告に対応

--- app/src/main/java/com/rydeenworks/mybooksearch/CalilWebViewClient.java
+++ app/src/main/java/com/rydeenworks/mybooksearch/CalilWebViewClient.java
@@ -1,35 +1,12 @@
 package com.rydeenworks.mybooksearch;
 
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class CalilWebViewClient extends WebViewClient {
     private BookLoadEventListener eventListener;
     public void SetEventListener(BookLoadEventListener listener) {
         eventListener = listener;
-    }
-
-    @Override
-    public void onPageStarted(WebView view, String url, Bitmap favicon) {
-        super.onPageStarted(view, url, favicon);
-    }
-    @Override
-    public void onPageFinished(WebView view, String url) {
-        super.onPageFinished(view, url);
-    }
-
-    @Override
-    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-        super.onReceivedError(view, errorCode, description, failingUrl);
-    }
-
-    @Override
-    public boolean shouldOverrideUrlLoading(android.webkit.WebView view, java.lang.String url) {
-        Uri uri = Uri.parse(url);
-        handleOverrideUrlLoading(uri);
-        return true;
     }
 
     @Override
