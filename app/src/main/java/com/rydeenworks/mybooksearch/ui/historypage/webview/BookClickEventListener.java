4d77eef2dc1475953b166648efa461a07cd5eecc
RydeenWorks <rydeenworks@gmail.com>
2023-01-14 20:46:07 +0900

    不要ファイル削除

--- app/src/main/java/com/rydeenworks/mybooksearch/ui/historypage/webview/BookClickEventListener.java
+++
@@ -1,9 +0,0 @@
-package com.rydeenworks.mybooksearch.ui.historypage.webview;
-
-import android.net.Uri;
-
-import java.util.EventListener;
-
-public interface BookClickEventListener extends EventListener {
-    void OnLinkClick(Uri uri);
-}
